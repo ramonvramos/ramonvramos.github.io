864d9f1544dcf88ebc7a630f04eda84a1272ff8d
Ramon Ramos <ramonramosrmt@gmail.com>
2023-02-16 23:33:34 +0800

    added mobile navbar, footer, & form submit function

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,24 @@
 const menuBox = document.querySelector(".menu__box");
+const mobileNav = document.querySelector(".mobile__menu");
+const darkBG = document.querySelector(".page__background");
 const menuContainer = document.querySelector(".menu__container");
 const navbar = document.querySelector(".style__header");
+const body = document.querySelector("body");
 
 // Mobile menu animation toggle
 menuBox.addEventListener("click", (e) => {
   menuContainer.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+  darkBG.classList.toggle("active");
+  body.classList.toggle("toHide");
 });
 
+darkBG.addEventListener("click", (e) => {
+  menuContainer.classList.remove("active");
+  mobileNav.classList.remove("active");
+  darkBG.classList.remove("active");
+  body.classList.remove("toHide");
+});
 // navbar animation slide up
 let previousScroll = window.pageYOffset;
 window.onscroll = function () {
